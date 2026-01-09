bd7becd02bf20576ec4864cb362b185b113ce844
Thaon-Chenimenil-ESTC <45682280+Thaon-Chenimenil-ESTC@users.noreply.github.com>
2026-01-09 14:12:51 +0100

    Update Joueurs.vue

--- src/components/Joueurs.vue
+++ src/components/Joueurs.vue
@@ -3,50 +3,6 @@
     <Card>
         <template #title>Joueurs</template>
         <template #content>
-            <div id="accordionExample" class="accordion">
-  <div class="card">
-    <div id="headingOne" class="card-header">
-      <h2 class="mb-0"><button class="btn btn-link btn-block text-left"
-          type="button" data-toggle="collapse" data-target="#collapseOne"
-          aria-expanded="true" aria-controls="collapseOne"> Collapsible Group
-          Item #1 </button></h2>
-    </div>
-    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
-      data-parent="#accordionExample">
-      <div class="card-body">Some placeholder content for the first accordion
-        panel. This panel is shown by default, thanks to the <code>.show</code>
-        class.</div>
-    </div>
-  </div>
-  <div class="card">
-    <div id="headingTwo" class="card-header">
-      <h2 class="mb-0"><button
-          class="btn btn-link btn-block text-left collapsed" type="button"
-          data-toggle="collapse" data-target="#collapseTwo"
-          aria-expanded="false" aria-controls="collapseTwo"> Collapsible Group
-          Item #2 </button></h2>
-    </div>
-    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo"
-      data-parent="#accordionExample">
-      <div class="card-body">Some placeholder content for the second accordion
-        panel. This panel is hidden by default.</div>
-    </div>
-  </div>
-  <div class="card">
-    <div id="headingThree" class="card-header">
-      <h2 class="mb-0"><button
-          class="btn btn-link btn-block text-left collapsed" type="button"
-          data-toggle="collapse" data-target="#collapseThree"
-          aria-expanded="false" aria-controls="collapseThree"> Collapsible Group
-          Item #3 </button></h2>
-    </div>
-    <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
-      data-parent="#accordionExample">
-      <div class="card-body">And lastly, the placeholder content for the third
-        and final accordion panel. This panel is hidden by default.</div>
-    </div>
-  </div>
-</div>
             <small>powered by <a target="_blank" href="https://www.pingpocket.fr/" >www.pingpocket.fr</a></small>
             <iframe frameborder="1" name="pingpocket" width="100%" height="800" scrolling="auto" src="https://www.pingpocket.fr/app/fftt/clubs/06880145/licencies?SORT=ALPHABETIC" ><p>iframe non supportée</p></iframe>
         </template>
